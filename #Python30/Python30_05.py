1f94d31a117d3ede1159965b6cbc4b359b79dca9
Carolina Acosta <carolina.acosta.m31@gmail.com>
2020-08-05 22:08:55 -0500

    Day 5: Filter

---
+++ #Python30/Python30_05.py
@@ -0,0 +1,48 @@
+"""
+Using the range function, create a sequence of numbers
+from 1 to 100, and using the filter function return only
+those that are multiplies of 2.
+"""
+def multiple_2(number):
+    return number % 2 == 0
+
+numbers = range(1,101)
+
+print(list(filter(multiple_2, numbers)))
+
+"""
+According to the example of older users, take the same
+list of users or create your own and return those who
+are underage.
+"""
+users = [
+    {
+        "name": "Esteban",
+        "age": 26
+    },
+    {
+        "name": "Jose",
+        "age": 15
+    },
+    {
+        "name": "Jaime",
+        "age": 18
+    }
+]
+
+def validate_age(user):
+    return user["age"] < 18
+
+print(list(filter(validate_age, users)))
+
+"""
+Create a new list of numbers in this case from -10 to
+10 and return only those that are multiples of 3 and
+negative.
+"""
+numbers = range(-10,10)
+
+def neg_multiple_3(number):
+    return (number % 3 == 0 and number < 0)
+
+print(list(filter(neg_multiple_3,numbers)))
